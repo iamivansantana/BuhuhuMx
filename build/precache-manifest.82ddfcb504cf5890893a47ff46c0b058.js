e5adfb46732947627763b58aa2f0e7d0526a00e6
ios dell <iam.ivansantana@gmail.com>
2021-02-28 14:33:57 -0600

    added file cpanel.yml

---
+++ build/precache-manifest.82ddfcb504cf5890893a47ff46c0b058.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bef3203a465531e5086810287165968e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f2f2b91c74669bdd492",
+    "url": "/static/css/main.01954e80.chunk.css"
+  },
+  {
+    "revision": "8225442178caf618a409",
+    "url": "/static/js/2.a3826a8a.chunk.js"
+  },
+  {
+    "revision": "fdc84dfb581e6fb8755dd1b2b01a50e1",
+    "url": "/static/js/2.a3826a8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f2f2b91c74669bdd492",
+    "url": "/static/js/main.967e62d6.chunk.js"
+  },
+  {
+    "revision": "002ea5d9f8bfec91122a",
+    "url": "/static/js/runtime-main.5f0d9bc7.js"
+  },
+  {
+    "revision": "85fa4356591206807effd0f8877dd3c3",
+    "url": "/static/media/gost.85fa4356.svg"
+  },
+  {
+    "revision": "b5c83383040a2b883d7e24b3cea75550",
+    "url": "/static/media/ios.b5c83383.png"
+  }
+]);
